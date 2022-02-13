cf93643a2c310198effe86f6ecf410d2f51b9d45
egi satria dharma y.w <76691272+egipapers321@users.noreply.github.com>
2022-02-13 22:06:25 +0700

    Update main.java

--- tugas/soal 6/main.java
+++ tugas/soal 6/main.java
@@ -5,7 +5,7 @@ package strings;
  <p>
  * author   : Egi Satria 
  <p>
- * since    : 2022-02-11
+ * since    : 2022-02-13
  <p>
  */
 import java.util.Scanner;
@@ -15,24 +15,22 @@ public class main{
     public static void main (String[] args){
 	
         String a,b,output; // deklarasi
-        int totalString;
 	
         a = keyboard.next ();
         b = keyboard.next ();
 	
-        totalString = a.length() + b.length(); //jumlah huruf
-        System.out.println(totalString);
+        System.out.println("jumlah hurufhe = "+(a.length() + b.length()));
 	
         if(a.compareTo(b) > 0){
           System.out.println("Yes");
         }
-        if(b.compareTo(a) < 0) {
+        else if(b.compareTo(a) > 0) {
            System.out.println("No");
         } else{
            System.out.println("A is equal to B");
         }
         // mengubah huruf depan
       output = a.substring(0,1).toUpperCase() + a.substring(1) + " " + b.substring(0,1).toUpperCase() + b.substring(1);
-      System.out.println("output");
+      System.out.println("output= " + output);
     }
 }
